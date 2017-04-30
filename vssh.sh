e6ecca2c92096809b1259e186ba4162ba2c18863
sbrumley <sbrumley@wireless-town.com>
2017-04-30 03:14:00 -0400

    comments

--- vssh.sh
+++ vssh.sh
@@ -4,4 +4,5 @@ if [[ "{$OS}" =~ "Windows" ]]; then
     cmd //C del %userprofile%\\.vagrant.d\\insecure_private_key
 fi
 
-vagrant up --provider=virtualbox && vagrant ssh
+vagrant up --provider=virtualbox && vagrant ssh  ## Build the Box and SSH to it
+vagrant package --output opendxl.box             ## Create a box to deploy on atlas
