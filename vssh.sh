816b6c69f2dc09dba7627f98d72538b8bfb205a1
sbrumley <sbrumley@wireless-town.com>
2017-04-30 03:33:49 -0400

    remove opendxl.box if it exists

--- vssh.sh
+++ vssh.sh
@@ -4,5 +4,6 @@ if [[ "{$OS}" =~ "Windows" ]]; then
     cmd //C del %userprofile%\\.vagrant.d\\insecure_private_key
 fi
 
+rm -rf opendxl.box
 vagrant up --provider=virtualbox && vagrant ssh  ## Build the Box and SSH to it
 vagrant package --output opendxl.box             ## Create a box to deploy on atlas
