81e66b1f8928a8e0cfd2a0583a24f16037605e2e
sbrumley <sbrumley@wireless-town.com>
2017-04-30 03:40:13 -0400

    breaking up shrinkBox command line

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -79,7 +79,8 @@ shrinkBox(){
     sudo apt-get clean
     sudo dd if=/dev/zero of=/EMPTY bs=1M
     sudo rm -f /EMPTY
-    cat /dev/null > ~/.bash_history && history -c && exit
+    sudo cat /dev/null > ~/.bash_history && history -c
+    exit
 }
 
 fixHostResolution
