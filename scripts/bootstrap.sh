9ab8c7c763205ae72fa32e195317e51af021fd82
sbrumley <sbrumley@wireless-town.com>
2017-05-01 08:35:53 -0400

    remove docker

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -123,14 +123,14 @@ installOpenDXLCLient
 checkOpenSSL
 installDos2Unix
 setupLogin
-fixVagrantSSHIssue
+#fixVagrantSSHIssue
 
 ## Only Install Docker if this is a vagrant VM
-if [[ "${ROOT_DIR}" == "/vagrant/" ]]; then
-    installDocker
-fi
+#if [[ "${ROOT_DIR}" == "/vagrant/" ]]; then
+#    installDocker
+#fi
 
 shrinkBox
-echo "Run sudo dd if=/dev/zero of=/EMPTY bs=1M && sudo rm -f /EMPTY to shrink VM"
+#echo "Run sudo dd if=/dev/zero of=/EMPTY bs=1M && sudo rm -f /EMPTY to shrink VM"
 echo "Type exit to leave VM.  Then ..."
 echo "Type vagrant package --output opendxl.box to build the box"
