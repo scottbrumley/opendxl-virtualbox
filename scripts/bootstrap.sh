1fa8a0301d5469ce2f5bdc3fc202934f7c4e27ee
sbrumley <sbrumley@wireless-town.com>
2017-05-01 08:37:09 -0400

    removed ssh fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -103,14 +103,6 @@ upgradePIP(){
     sudo pip install --upgrade pip
 }
 
-fixVagrantSSHIssue(){
-mkdir -p ~/.ssh
-sudo wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O ~/.ssh/authorized_keys
-sudo chmod 700 ~/.ssh
-sudo chmod 600 ~/.ssh/authorized_keys
-sudo chown -R vagrant:vagrant ~/.ssh
-
-}
 
 fixHostResolution
 aptCleanUp
@@ -123,7 +115,6 @@ installOpenDXLCLient
 checkOpenSSL
 installDos2Unix
 setupLogin
-#fixVagrantSSHIssue
 
 ## Only Install Docker if this is a vagrant VM
 #if [[ "${ROOT_DIR}" == "/vagrant/" ]]; then
