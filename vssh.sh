9944f99c21fb9342cea6dfeab9f4feca18e57276
sbrumley <sbrumley@wireless-town.com>
2017-05-18 12:24:23 -0400

    edit to allow any ROOT_DIR

--- vssh.sh
+++ vssh.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+rm -rf opendxl.box
 
 if [[ "{$OS}" =~ "Windows" ]]; then
     cmd //C del %userprofile%\\.vagrant.d\\insecure_private_key
 fi
 
-rm -rf opendxl.box
-vagrant up --provider=virtualbox && vagrant ssh  ## Build the Box and SSH to it
-#vagrant package --output opendxl.box             ## Create a box to deploy on atlas
+if [ "${1}" == "vsphere" ]; then
+    echo "Setup Vsphere"
+    vagrant plugin install vagrant-vsphere
+    VAGRANT_VAGRANTFILE=vsphere vagrant up --provider=vsphere && vagrant ssh
+else
+   echo "Setup Virtualbox"
+    vagrant up --provider=virtualbox && vagrant ssh
+fi
