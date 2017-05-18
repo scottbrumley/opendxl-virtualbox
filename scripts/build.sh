edb591401cb2b6b8eb21d6f422e40d7face12eed
sbrumley <sbrumley@wireless-town.com>
2017-05-18 13:21:29 -0400

    Added push to Docker Hub option to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker build .
+
+
+if [ "${1}" == "push" ]; then
+    echo "Building and Pushing Container to Docker Hub"
+    docker build -t "mcafee:opendxl" .
+    docker login
+    docker tag mcafee:opendxl sbrumley/opendxl
+    docker push sbrumley/opendxl
+else
+   echo "Building Locally"
+   docker build -t "sbrumley:opendxl" .
+fi
